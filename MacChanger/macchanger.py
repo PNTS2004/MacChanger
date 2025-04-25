a8f07b8c3d27701b2c1eea7c7890dd35325489d4
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-04-25 14:19:58 -0400

    Initializing variables based on Command-Line Arguments

--- MacChanger/macchanger.py
+++ MacChanger/macchanger.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface=input('Interface: ')
-new_mac=input('New Mac Address: ')
+parser= optparse.OptionParser()
+
+parser.add_option('-i', '--interface', dest='interface', help='Interface to change its Mac Address')
+parser.add_option('-m', '--mac', dest='new_mac', help='New Mac Address')
+
+(options, arguments) = parser.parse_args()
+
+interface=options.interface
+new_mac=options.new_mac
+
 print('changing mac address')
 subprocess.call('ifconfig '+interface+' down', shell=True)
 subprocess.call('ifconfig '+interface+' hw ether '+new_mac, shell=True)
